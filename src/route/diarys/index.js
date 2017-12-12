fc50fd534411dfd154c56a9538b5e7222fcc72ae
jinhoKong <tpdleps@gmail.com>
2017-12-12 13:39:11 +0900

    Diary router init

---
+++ src/route/diarys/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('./diary.controller');
+ 
+// Route Setting
+router.get('/', ctrl.diarys);
+router.get('/:diarycode', ctrl.select);
+router.post('/', ctrl.create);
+router.put('/:diarycode', ctrl.update);
+router.delete('/:diarycode', ctrl.destory);
+
+module.exports = router;
